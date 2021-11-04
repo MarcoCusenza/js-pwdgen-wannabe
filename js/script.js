6d2e8be02663c4b9e374f42cb0faad5c14b8819b
Marco <marco.cusenza.me@gmail.com>
2021-11-04 18:14:03 +0100

    fine js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 alert("Hello World!");
 
-let nome = prompt("Inserisci il tuo nome");
-let cognome = prompt("Inserisci il tuo cognome");
-let colorePref = prompt("Inserisci il tuo colore preferito");
+const nome = prompt("Inserisci il tuo nome");
+document.getElementById("name").innerHTML = nome;
+
+const cognome = prompt("Inserisci il tuo cognome");
+document.getElementById("surname").innerHTML = cognome;
+
+const colorePref = prompt("Inserisci il tuo colore preferito");
+document.getElementById("fav-color").innerHTML = colorePref;
+
+let pwd = nome + cognome + colorePref + '21'
+document.getElementById("pwd").innerHTML = pwd;
