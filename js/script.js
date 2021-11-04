19725f119555157de72bf775d42d6f1034dcfc95
Marco <marco.cusenza.me@gmail.com>
2021-11-04 18:25:44 +0100

    aggiunta condizioni

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert("Hello World!");
+// alert("Hello World!");
 
 const nome = prompt("Inserisci il tuo nome");
 document.getElementById("name").innerHTML = nome;
@@ -9,5 +9,11 @@ document.getElementById("surname").innerHTML = cognome;
 const colorePref = prompt("Inserisci il tuo colore preferito");
 document.getElementById("fav-color").innerHTML = colorePref;
 
-let pwd = nome + cognome + colorePref + '21'
-document.getElementById("pwd").innerHTML = pwd;
+if (nome == "" || cognome == "" || colorePref == "") {
+    document.getElementById("pwd").innerHTML = "ATTENZIONE: non hai inserito tutti i dati correttamente";
+}
+else {
+    document.getElementById("pwd-title").innerHTML = "La tua nuova password è:";
+    let pwd = nome + cognome + colorePref + '21'
+    document.getElementById("pwd").innerHTML = pwd;
+}
