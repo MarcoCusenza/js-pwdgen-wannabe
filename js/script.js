e805f6e666b2e662568e7717d6cd9651fb7ba9d6
Marco <marco.cusenza.me@gmail.com>
2021-11-04 17:46:30 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+alert("Hello World!");
+
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colorePref = prompt("Inserisci il tuo colore preferito");
